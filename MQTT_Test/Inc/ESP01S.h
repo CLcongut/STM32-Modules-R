fcff28746723ebe8a008dd37cba26c0a33752a0d
CLcongut <13655822806@163.com>
2024-05-20 02:32:48 +0800

    提交各种模块驱动代码HAL

---
+++ MQTT_Test/Inc/ESP01S.h
@@ -0,0 +1,7 @@
+#ifndef ESP01S_H
+#define ESP01S_H
+
+void _t_printcmd(void);
+void ESP_Init(void);
+
+#endif
---
+++ MQTT_Test/Src/ESP01S.c
@@ -0,0 +1,155 @@
+#include "main.h"
+#include "ESP01S.h"
+#include "usart.h"
+#include "lcd.h"
+
+#define ESP_Transmit(_cmd) printf(_cmd)
+#define ESP_Transmit_long(_cmd) HAL_UART_Transmit(&huart1, (unsigned char *)_cmd, strlen(_cmd), 0xFFFF);
+
+#define wifi_SSID "CLcongut"
+#define wifi_PSWD "qwertyui"
+
+#define mqtt_USERNAME "dht11&a15ZWaMgl9e"
+#define mqtt_PASSWORD "4FF383A5FD3E9567CF41EF11A84DA9E6E852B606"
+#define mqtt_CLIENTID "30383|securemode=3\\,signmethod=hmacsha1|"
+#define mqtt_HOST_URL "a15ZWaMgl9e.iot-as-mqtt.cn-shanghai.aliyuncs.com"
+
+#define mqtt_pub_TOPIC "/sys/a15ZWaMgl9e/${deviceName}/thing/event/property/post"
+#define mqtt_sub_TOPIC "/sys/a15ZWaMgl9e/${deviceName}/thing/service/property/set"
+
+#define cmd_RST "AT+RST\r\n"
+#define cmd_REPLY "ATE0\r\n"
+#define cmd_CWQAP "AT+CWQAP\r\n"
+#define cmd_CWMODE "AT+CWMODE=3\r\n"
+#define cmd_AUTOCONN "AT+CWAUTOCONN=0\r\n"
+#define cmd_CIPSNTPCFG "AT+CIPSNTPCFG=1,8,\"cn.ntp.org.cn\",\"ntp.sjtu.edu.cn\"\r\n"
+/*ESP01S模块供电不足容易导致无法连接AP*/
+#define cmd_CWJAP "AT+CWJAP=\"" wifi_SSID "\",\"" wifi_PSWD "\"\r\n"
+#define cmd_MQTTUSERCFG "AT+MQTTUSERCFG=0,1,\"NULL\",\"" mqtt_USERNAME "\",\"" mqtt_PASSWORD "\",0,0,\"\"\r\n"
+#define cmd_MQTTCLIENTID "AT+MQTTCLIENTID=0,\"" mqtt_CLIENTID "\"\r\n"
+#define cmd_MQTTCONN "AT+MQTTCONN=0,\"" mqtt_HOST_URL "\",1883,1\r\n"
+
+void gui_dp_info(const uint8_t *_info)
+{
+    LCD_Fill(0, 0, 128, 12, WHITE);
+    LCD_ShowString(0, 0, _info, BLACK, WHITE, 12, 0);
+}
+
+void ESP_Judge(const char *message)
+{
+    uint8_t data[25];
+    /*全局结构体成员，使用也要创个变量来赋值，不可直接使用，更不能强制转换*/
+    uint8_t *data_rx;
+    while (1)
+    {
+        data_rx = RX_DS.rx_data;
+        /*使用 %*[^\r\n] 来跳过内容时，必须在\r\n前还有内容，否则会卡住，不如直接使用以下方式*/
+        sscanf((const char *)data_rx, "\r\n%s\r\n", data); // sscanf((const char *)RX_DS.rx_data, "%*[^\r\n]%s\r\n", data);
+        // LCD_Fill(0, 20, 128, 32, WHITE);
+        // LCD_ShowString(0, 20, data_rx, BLACK, WHITE, 12, 0);
+        if (memcmp((const char *)data, message, strlen(message)) == 0)
+        {
+            break;
+        }
+    }
+}
+
+void ESP_Init(void)
+{
+#if 1
+    /**
+     * @brief 延时稳定，准备
+     *
+     */
+    HAL_Delay(1500);
+    gui_dp_info("READY FOR ESP01");
+
+    /**
+     * @brief 关闭回显
+     *
+     */
+    ESP_Transmit(cmd_REPLY);
+    HAL_Delay(1500);
+    gui_dp_info("CLOSE REPLY");
+
+    /**
+     * @brief 关闭AP连接
+     *
+     */
+    HAL_Delay(1500);
+    ESP_Transmit(cmd_CWQAP);
+    HAL_Delay(1500);
+    gui_dp_info("WIFI DISCONN");
+
+    /**
+     * @brief 重启ESP01S
+     *
+     */
+    HAL_Delay(1500);
+    ESP_Transmit(cmd_RST);
+    gui_dp_info("RESET ESP01");
+    HAL_Delay(3000);
+
+    /**
+     * @brief 关闭回显
+     *
+     */
+    ESP_Transmit(cmd_REPLY);
+    HAL_Delay(1500);
+    ESP_Judge("OK");
+    gui_dp_info("CLOSE REPLY");
+
+    /**
+     * @brief 切换模式 1 station模式
+     *
+     */
+    ESP_Transmit(cmd_CWMODE);
+    HAL_Delay(1500);
+    ESP_Judge("OK");
+    gui_dp_info("SET ESP01 MODE");
+
+    /**
+     * @brief 关闭上电自动连接AP
+     *
+     */
+    ESP_Transmit(cmd_AUTOCONN);
+    HAL_Delay(1500);
+    ESP_Judge("OK");
+    gui_dp_info("CLOSE AUTO CONN");
+
+    /**
+     * @brief 设置时区服务器
+     *
+     */
+    ESP_Transmit(cmd_CIPSNTPCFG);
+    HAL_Delay(1500);
+    ESP_Judge("OK");
+    gui_dp_info("SET TIME ZONE");
+
+    /**
+     * @brief 设置AP名字和密码，并且连接
+     *
+     */
+    ESP_Transmit(cmd_CWJAP);
+    HAL_Delay(1500);
+    gui_dp_info("SET WIFI CONN");
+    HAL_Delay(3000);
+    ESP_Judge("OK");
+    gui_dp_info("WIFI CONNECT");
+#endif
+
+    ESP_Transmit(cmd_MQTTUSERCFG);
+    HAL_Delay(1500);
+    ESP_Judge("OK");
+    gui_dp_info("SET MQTT USERCFG");
+
+    ESP_Transmit(cmd_MQTTCLIENTID);
+    HAL_Delay(1500);
+    ESP_Judge("OK");
+    gui_dp_info("SET MQTT CLINTID");
+
+    ESP_Transmit(cmd_MQTTCONN);
+    HAL_Delay(1500);
+    ESP_Judge("OK");
+    gui_dp_info("SET MQTT URL");
+}
